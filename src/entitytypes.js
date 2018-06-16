0da3f1a54dfbbca2428528779f4f87a1c3054775
Candle <candle@candle.me.uk>
2018-06-16 10:38:13 +0100

    multiple unit tests for connections between entities; and as simple an implementation as possible to make them pass.

--- src/entitytypes.js
+++ src/entitytypes.js
@@ -340,6 +340,7 @@ const Recipe = (superclass) => class extends superclass {
 class Connection {
   // side, colour ==> entity
   // colour is optional (see Cu0/Cu1 for manual copper connections - mostly found in power switches)
+  // theirSide is also optional; Cu0/Cu1 don't show up on the other side
   constructor(otherEntity, mySide, theirSide, colour) {
     this.otherEntity = otherEntity;
     this.mySide = mySide;
@@ -403,18 +404,16 @@ const Connections = (superclass) => class extends superclass {
 
     updateConnections(entityNumberMap) {
       if (typeof _rawConnections === 'undefiend') return; // early exit for entities that have no connections.
+      // Example _rawConnections
+      // {
+      //   '1': { red: [ { entity_id: 1 } ] },
+      //   'Cu1': [ { entity_id: 3, wire_id: 0 } ] }
+      // }
       const list = [];
-//        connections: {
-//          '1': {
-//            red: [
-//              { entity_id: 1 }
-//            ]
-//          },
-//          'Cu1': [ { entity_id: 3, wire_id: 0 } ] }
-//        }
 
       // function to parse the list of connected entities then create and store
-      // the Connection objects.
+      // the Connection objects. As a function because the arary to handle
+      // can be located at different places in the object.
       const handleEntityConnectionList = (l, ms, ts, c) => {
         l.forEach(entityObj => {
           list.push(new Connection(entityNumberMap[entityObj.entity_id], ms, ts, c));
@@ -423,15 +422,15 @@ const Connections = (superclass) => class extends superclass {
 
       // read the connections block and flatten to lists of connections
       Object.keys(this._rawConnections).forEach(side => {
-        if (typeof this._rawConnections[side] === 'object') {
+        if (Array.isArray(this._rawConnections[side])) {
+          // Cu0/1; copper connections; usually found in power switches.
+          // XXX not sure what the wire_id field is for, so ignoring for now.
+          handleEntityConnectionList(this._rawConnections[side], side, null);
+        } else {
           // in(1)/out(2); usually red & green wires.
           Object.keys(this._rawConnections[side]).forEach(colour => {
             handleEntityConnectionList(this._rawConnections[side][colour], side, null, colour);
           });
-        } else {
-          // Cu0/1; copper connections; usually found in power switches.
-          // XXX not sure what the wire_id field is for, so ignoring for now.
-          handleEntityConnectionList(this._rawConnections[side], side, null);
         }
       });
 
@@ -440,7 +439,20 @@ const Connections = (superclass) => class extends superclass {
 
     toObject() {
       const mine = {
+        connections: {}
       };
+      this._connections.table.forEach(c => {
+        var arr;
+        if (typeof c.colour === 'undefined') {
+          if (typeof mine.connections[c.mySide] === 'undefined') mine.connections[c.mySide] = [];
+          arr = mine.connections[c.mySide];
+        } else {
+          if (typeof mine.connections[c.mySide] === 'undefined') mine.connections[c.mySide] = {};
+          if (typeof mine.connections[c.mySide][c.colour] === 'undefined') mine.connections[c.mySide][c.colour] = [];
+          arr = mine.connections[c.mySide][c.colour];
+        }
+        arr.push({ entity_id: c.otherEntity.number() });
+      });
 
       const sup = (super.toObject) ? super.toObject() : {};
       return {...sup, ...mine};
