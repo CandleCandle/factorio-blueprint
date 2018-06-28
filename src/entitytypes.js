c13c159bdaa46cd93b40511df75e701510846225
Candle <candle@candle.me.uk>
2018-06-28 19:01:10 +0100

    More comments on the current problems.

--- src/entitytypes.js
+++ src/entitytypes.js
@@ -4,7 +4,7 @@
 const Victor = require('victor');
 const mixwith = require('mixwith');
 
-/* Current problem:
+/* Current problems:
  * * tileDataAction *
  * tileDataAction needs to know about: position, size and direction.
  * Option 1: use if (typeof this.{function} === 'function') to detect that the resulting class is mixed with the appropriate other mixin.
@@ -24,6 +24,47 @@ const mixwith = require('mixwith');
  * position depends-on size
  * tileDataAction depends-on position
  * tileDataAction depends-on size
+ *
+ *
+ *
+ * * Item Types stored using underscore based names *
+ * The raw blueprint JSON representation stores item type names using hyphens
+ * to separate words; e.g. filter-stack-inserter, or speed-module-3.
+ *
+ * Option 1: backwards compatibility
+ * Convert all - to _ on reading, use _ throughout the code. convert to - on
+ * output.
+ * Leads to complicated code in many places, dealing with the conversion; particularly
+ * when items are object field names.
+ *
+ * Option 2: keep using - and break backwards compatibility
+ * Use - throughout the code. defaultentities.js would have to change; and can
+ * if the entries become strings and are quoted like:
+ * `'assembling-machine-3': { modules: 4 }`
+ *
+ *
+ *
+ * * Field access on the Entity class *
+ * The original Entity class exposes multiple fields to users; for example the
+ * 'modules' field. When this is modified to add/remove modules, it bypasses any
+ * encapsulation that the Entity class offers; Sample code might:
+ * const e = blueprint.createEntity(...);
+ * e.modules.speed_module_3 = 3;
+ *
+ * This bypasses any checks that might happen on the maximum allowed modules,
+ * leading to having to check the module count at toObject time, moving the error
+ * from the place it actually occurs to effectivly a random place.
+ *
+ * This also means that we cannot store the type of modules using the in-game
+ * names (hyphens and not underscores)
+ *
+ * from the documentation:
+ * Do not modify properties directly! Functions to set things such as direction or position are included because they do more than just set the variable.
+ *
+ * I think this statement allows me to change the internal behaviour and mutate
+ * using 'get' methods in the compatibility layer.
+ * 
+ *
  */
 
 
@@ -239,7 +280,7 @@ const Filter = (superclass) => class extends superclass {
     }
 
     addFilter(type) {
-      // find the next available slot for type.
+      // TODO find the next available slot for type; filling gaps as required
       this._filters[this._filters.size] = type;
       return this;
     }
