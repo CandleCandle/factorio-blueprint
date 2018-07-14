d8bd54892fb9dc5e31a563689994ec3a796ae570
Candle <candle@candle.me.uk>
2018-07-14 21:12:54 +0100

    packaging updates

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,5 @@ module.exports = {
         filename: './dist/factorio-blueprint.min.js',
         library: 'Blueprint',
         libraryTarget: 'umd'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['babel-preset-env']
-                    }
-                }
-            }
-        ]
     }
 }
