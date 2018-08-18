a66619d52939caf1751ca01fb776f9abde334a13
Candle <candle@candle.me.uk>
2018-08-18 16:29:11 +0100

    straight rails can identify where rails can be places next to them so that they connect.

--- src/entitymixins/straightrail.js
+++ src/entitymixins/straightrail.js
@@ -26,6 +26,58 @@ const StraightRail = (superclass) => class extends superclass {
         fn(central.x+1, central.y);
       }
     }
+
+  /**
+   * returns an object with the keys being a direction; meaning "in this direction,
+   * there can be a rail piece". The value is an object describing the position
+   * and direction of the new rail piece.
+   */
+  adjacentStraight() {
+    const result = {};
+    const d = this.direction();
+    console.log('this', this);
+    if (d === 0 || d === 4) {
+      result[0] = {
+        position: this.position().clone().add({x: 0, y: -2}),
+        direction: d
+      };
+      result[4] = {
+        position: this.position().clone().add({x: 0, y: +2}),
+        direction: d
+      };
+    }
+    if (d === 1 || d === 5) {
+      result[1] = {
+        position: this.position().clone().add({x: +1, y: -1}),
+        direction: (d+4) % 8
+      };
+      result[5] = {
+        position: this.position().clone().add({x: -1, y: +1}),
+        direction: (d+4) % 8
+      };
+    }
+    if (d === 2 || d === 6) {
+      result[2] = {
+        position: this.position().clone().add({x: +2, y: 0}),
+        direction: d
+      };
+      result[6] = {
+        position: this.position().clone().add({x: -2, y: 0}),
+        direction: d
+      };
+    }
+    if (d === 3 || d === 7) {
+      result[3] = {
+        position: this.position().clone().add({x: +1, y: +1}),
+        direction: (d+4) % 8
+      };
+      result[7] = {
+        position: this.position().clone().add({x: -1, y: -1}),
+        direction: (d+4) % 8
+      };
+    }
+    return result;
+  }
   };
 
 module.exports = StraightRail;
--- test/straightrail_tests.js
+++ test/straightrail_tests.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const entity = require('../src/entitytypes');
 const Victor = require('victor');
 const mixwith = require('mixwith');
+const Blueprint = require('../src/index');
 
 
 
@@ -80,6 +81,76 @@ describe('Entity Features', function () {
       assert.equal(visited["11,12"], 1);
       assert.equal(visited["10,11"], 1);
       assert.equal(visited["12,11"], 1);
+    });
+  });
+
+
+  describe('straignt rail segments', function () {
+    const StraightRailEntity = mixwith.mix(entity.BaseEntity).with(entity.Direction, entity.Position, entity.StraightRail);
+
+    describe('adjacent rail segments', function () {
+      it('directions 0 and 4 are up&down and preserve entity direction', function () {
+        const entity = new StraightRailEntity()
+              .name('straight-rail')
+              .position(new Victor(12, 12))
+              .width(2).height(2)
+              .direction(0);
+        const result = entity.adjacentStraight();
+        console.log(entity);
+        console.log(result);
+        assert.equal(result[Blueprint.UP].position.x, 12);
+        assert.equal(result[Blueprint.UP].position.y, 10);
+        assert.equal(result[Blueprint.UP].direction, 0, 'up, direction ' + result[Blueprint.UP].d);
+        assert.equal(result[Blueprint.DOWN].position.x, 12);
+        assert.equal(result[Blueprint.DOWN].position.y, 14);
+        assert.equal(result[Blueprint.DOWN].direction, 0, 'down, direction');
+        [1,2,3,5,6,7].forEach(d => assert.equal(typeof result[d], 'undefined', "expected "+d+" to not be defined, was "+result[d]));
+      });
+      it('directions 2 and 6 are right&left and preserve entity direction', function () {
+        const entity = new StraightRailEntity()
+              .name('straight-rail')
+              .position(new Victor(12, 12))
+              .width(2).height(2)
+              .direction(2);
+        const result = entity.adjacentStraight();
+        assert.equal(result[Blueprint.LEFT].position.x, 10);
+        assert.equal(result[Blueprint.LEFT].position.y, 12);
+        assert.equal(result[Blueprint.LEFT].direction, 2);
+        assert.equal(result[Blueprint.RIGHT].position.x, 14);
+        assert.equal(result[Blueprint.RIGHT].position.y, 12);
+        assert.equal(result[Blueprint.RIGHT].direction, 2);
+        [0,1,3,4,5,7].forEach(d => assert.equal(typeof result[d], 'undefined', "expected "+d+" to not be defined, was "+result[d]));
+      });
+      it('directions 1 and 5 are NW&SE and opposite entity direction', function () {
+        const entity = new StraightRailEntity()
+              .name('straight-rail')
+              .position(new Victor(12, 12))
+              .width(2).height(2)
+              .direction(1);
+        const result = entity.adjacentStraight();
+        assert.equal(result[1].position.x, 13);
+        assert.equal(result[1].position.y, 11);
+        assert.equal(result[1].direction, 5);
+        assert.equal(result[5].position.x, 11);
+        assert.equal(result[5].position.y, 13);
+        assert.equal(result[5].direction, 5);
+        [0,2,3,4,6,7].forEach(d => assert.equal(typeof result[d], 'undefined', "expected "+d+" to not be defined, was "+result[d]));
+      });
+      it('directions 3 and 7 are NE&SW and opposite entity direction', function () {
+        const entity = new StraightRailEntity()
+              .name('straight-rail')
+              .position(new Victor(12, 12))
+              .width(2).height(2)
+              .direction(7);
+        const result = entity.adjacentStraight();
+        assert.equal(result[3].position.x, 13);
+        assert.equal(result[3].position.y, 13);
+        assert.equal(result[3].direction, 3);
+        assert.equal(result[7].position.x, 11);
+        assert.equal(result[7].position.y, 11);
+        assert.equal(result[7].direction, 3);
+        [0,1,2,4,5,6].forEach(d => assert.equal(typeof result[d], 'undefined', "expected "+d+" to not be defined, was "+result[d]));
+      });
     });
   });
 });
